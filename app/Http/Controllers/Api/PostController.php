18971c4d6c2896b5b218be16aa27c04b8489209f
SaBobic <fabrizioettori@gmail.com>
2022-10-04 16:52:38 +0200

    added paginate method into api.php

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::with('category', 'tags', 'user')->orderBy('updated_at', 'DESC')->get();
+        $posts = Post::with('category', 'tags', 'user')->orderBy('updated_at', 'DESC')->paginate(12);
         return response()->json($posts);
     }
 
