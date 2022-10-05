e6b780ed4d7bfd3efd390f3311023a6adc84b99e
SaBobic <fabrizioettori@gmail.com>
2022-10-05 16:23:54 +0200

    added Api\PostController@show api by slug

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -36,9 +36,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        //
+        $post = Post::with('category', 'tags', 'user')->where('slug', $slug)->get();
+        return response()->json($post);
     }
 
     /**
